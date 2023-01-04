dcccb9045c11bb22b26d22f6c97889d883c7f474
agungSeptiana <203040102@mail.unpas.ac.id>
2023-01-04 12:09:01 +0700

    Final project

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -21,7 +21,6 @@ class ProductController extends Controller
      */
     public function index()
     {   
-        abort_if(Gate::denies('product_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         $products = Product::with('category', 'tags', 'firstMedia')->latest()->paginate(5); 
 
@@ -35,7 +34,6 @@ class ProductController extends Controller
      */
     public function create()
     {
-        abort_if(Gate::denies('product_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         $categories = Category::latest()->get(['id', 'name']);
         $tags = Tag::latest()->get(['id', 'name']);
@@ -51,7 +49,6 @@ class ProductController extends Controller
      */
     public function store(ProductRequest $request)
     {
-        abort_if(Gate::denies('product_create'), Response::HTTP_FORBIDDEN, '403 Forbidden');
        
         if ($request->validated()){
             $product = Product::create($request->except('tags', 'images', '_token'));
@@ -81,8 +78,6 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        abort_if(Gate::denies('product_view'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         return view('admin.products.show', compact('product'));
     }
 
@@ -93,9 +88,7 @@ class ProductController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit(Product $product)
-    {
-        abort_if(Gate::denies('product_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-        
+    {   
         $categories = Category::latest()->get(['id', 'name']);
         $tags = Tag::latest()->get(['id', 'name']);
 
@@ -111,8 +104,6 @@ class ProductController extends Controller
      */
     public function update(ProductRequest $request,Product $product)
     {
-        abort_if(Gate::denies('product_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         if ($request->validated()) {
             $product->update($request->except('tags', 'images', '_token'));
             $product->tags()->sync($request->tags);
@@ -143,8 +134,6 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        abort_if(Gate::denies('product_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-
         if ($product->media->count() > 0) {
             foreach ($product->media as $media) {
                 (new ImageService())->unlinkImage($media->file_name, 'products');
@@ -161,9 +150,7 @@ class ProductController extends Controller
     }
 
     public function removeImage(Request $request)
-    {
-        abort_if(Gate::denies('product_delete'), Response::HTTP_FORBIDDEN, '403 Forbidden');
-       
+    {  
         $product = Product::findOrFail($request->product_id);
         $image = $product->media()->whereId($request->image_id)->first();
 
